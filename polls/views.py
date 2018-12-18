c74d2da99c71d1338254d4e734538762425e6c20
wuhz <lorl_wuhz@outlook.com>
2018-12-18 08:53:18 +0800

    modified polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRespose
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
